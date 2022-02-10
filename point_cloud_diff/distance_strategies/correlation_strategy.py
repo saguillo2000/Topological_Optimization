dbacece06c320df376049834a70389ee5282073a
saguillo2000 <oriolsaguilo@gmail.com>
2022-02-10 10:43:09 +0100

    Small Refactor of Strategies

--- point_cloud_diff/distance_strategies/correlation_strategy.py
+++ point_cloud_diff/distance_strategies/correlation_strategy.py
@@ -4,18 +4,6 @@ from sklearn.metrics.pairwise import euclidean_distances
 import tensorflow_probability as tfp
 
 
-def distance_corr(X: tf.Tensor) -> tf.Tensor:
-    correlation = np.corrcoef(X.numpy())
-    np.nan_to_num(correlation, copy=False)
-    return tf.convert_to_tensor(correlation, np.float64)
-
-
-def distance_corr_ruben(X):
-    correlation = np.sqrt(1 - np.power(np.corrcoef(X), 2))
-    np.nan_to_num(correlation, copy=False)
-    return correlation
-
-
 def euclidean_distance(X: tf.Tensor) -> tf.Tensor:
     euclidean = euclidean_distances(X, X)
     return tf.convert_to_tensor(euclidean, tf.float32)
@@ -25,11 +13,11 @@ def correlation_numpy(x):
     return np.sqrt(1 - np.power(np.corrcoef(x), 2))
 
 
-def replacenan(t):
+def replace_nan(t):
     return tf.where(tf.math.is_nan(t), tf.zeros_like(t), t)
 
 
 def distance_corr_tf(x):
     y = tf.math.sqrt(1 - tf.math.pow(tfp.stats.correlation(tf.transpose(x), sample_axis=0, event_axis=-1), 2))
     # return tf.numpy_function(correlation_numpy, [input], tf.float32)
-    return replacenan(y)
+    return replace_nan(y)
