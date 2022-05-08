781940a3995ce9bcd3a685e23152eb1c4fed0ab1
saguillo2000 <oriolsaguilo@gmail.com>
2022-05-08 16:58:13 +0200

    Datasets finished

--- tests/datasets.py
+++ tests/datasets.py
@@ -2,28 +2,39 @@ from tensorflow.keras import datasets
 
 
 def dataset_MNIST():
-    PROBLEM_INPUT_SIZE = (28, 28)
-    PROBLEM_OUTPUT_SIZE = 10
+    INPUT_SIZE = (28, 28)
+    OUTPUT_SIZE = 10
 
     (train_images, train_labels), (test_images, test_labels) = datasets.mnist.load_data()
     train_images, test_images = train_images / 255.0, test_images / 255.0
 
-    return 0
+    train = (train_images, train_labels)
+    test = (test_images, test_labels)
+
+    return train, test, INPUT_SIZE, OUTPUT_SIZE
 
 
 def dataset_CIFAR10():
-    PROBLEM_INPUT_SIZE = (32, 32, 3)
-    PROBLEM_OUTPUT_SIZE = 10
+    INPUT_SIZE = (32, 32, 3)
+    OUTPUT_SIZE = 10
 
     (train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
     train_images, test_images = train_images / 255.0, test_images / 255.0
-    return 0
+
+    train = (train_images, train_labels)
+    test = (test_images, test_labels)
+
+    return train, test, INPUT_SIZE, OUTPUT_SIZE
 
 
 def dataset_CIFAR100():
-    PROBLEM_INPUT_SIZE = (32, 32, 3)
-    PROBLEM_OUTPUT_SIZE = 100
+    INPUT_SIZE = (32, 32, 3)
+    OUTPUT_SIZE = 100
 
     (train_images, train_labels), (test_images, test_labels) = datasets.cifar100.load_data()
     train_images, test_images = train_images / 255.0, test_images / 255.0
-    return 0
+
+    train = (train_images, train_labels)
+    test = (test_images, test_labels)
+
+    return train, test, INPUT_SIZE, OUTPUT_SIZE
--- tests/train_loss_test.py
+++ tests/train_loss_test.py
@@ -1,5 +1,15 @@
 from tests.models import *
 from tests.datasets import *
+from topo_loss_train.train import *
+
+
+def training():
+    return 0
+
 
 if __name__ == '__main__':
-    print('Hello World')
+    train_10, test_10, INPUT_SIZE_10, OUTPUT_SIZE_10 = dataset_CIFAR10()
+    train_100, test_100, INPUT_SIZE_100, OUTPUT_SIZE_100 = dataset_CIFAR100()
+    train_MNIST, test_MNSIT, INPUT_SIZE_MNIST, OUTPUT_SIZE_MNIST = dataset_MNIST()
+
+
